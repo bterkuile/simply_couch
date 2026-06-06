5ed9ab99ee6176d4d7ed03fad9e156a44c3054cf
BenClaw <benclaw@mozo.bar>
2026-06-07 00:45:00 +0200

    fix: before_validation_on_save → before_validation in Callbacker fixture

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,7 @@ RSpec.configure do |config|
     rescue StandardError
       # database doesn't exist or already deleted
     end
-    server.create_db(TEST_DB)
+    begin; server.create_db(TEST_DB); rescue RestClient::PreconditionFailed, CouchRest::PreconditionFailed, CouchRest::NotFound; end
     SimplyStored::Couch::Database.class_eval do
       define_method(:couchrest_database_url) { "#{COUCHDB_URL}/#{TEST_DB}" }
     end
