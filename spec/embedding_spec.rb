826bcce1f7d48c98f72f1b15f2b981fd917c3666
BenClaw <benclaw@mozo.bar>
2026-06-08 17:19:55 +0200

    Use single quotes and key: value syntax in spec files

--- spec/embedding_spec.rb
+++ spec/embedding_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-describe "Embedding" do
-  context "initialized comments" do
+describe 'Embedding' do
+  context 'initialized comments' do
     before do
       @post = Post.new
       @post.embedded_comments = [
@@ -11,32 +11,32 @@ describe "Embedding" do
       @post.save
     end
 
-    it "return a valid size" do
+    it 'return a valid size' do
       expect(@post.embedded_comments.size).to eq 2
       post_reloaded = Post.find(@post.id)
       expect(post_reloaded.embedded_comments.size).to eq 2
     end
 
-    it "delete comment using object" do
+    it 'delete comment using object' do
       @post.remove_embedded_comment(@post.embedded_comments.first)
       expect(@post.embedded_comments.size).to eq 1
       post_reloaded = Post.find(@post.id)
       expect(post_reloaded.embedded_comments.size).to eq 1
     end
 
-    it "get all emmbedded using .all" do
+    it 'get all emmbedded using .all' do
       expect(EmbeddedComment.all.size).to eq 2
     end
 
-    it "get embedded object, not a hash" do
+    it 'get embedded object, not a hash' do
       expect(EmbeddedComment.all.first).to be_a EmbeddedComment
     end
 
-    it "have a parent_object when loaded through all" do
+    it 'have a parent_object when loaded through all' do
       expect(EmbeddedComment.all.first.parent_object).to eq @post
     end
 
-    it "save an instance" do
+    it 'save an instance' do
       comment = @post.embedded_comments.first
       comment.body = 'body-changed'
       comment.save
@@ -44,7 +44,7 @@ describe "Embedding" do
       expect(comment_reloaded.body).to eq 'body-changed'
     end
 
-    it "change attribute when not loaded through parent object" do
+    it 'change attribute when not loaded through parent object' do
       embedded_comments = EmbeddedComment.all
       embedded_comment = embedded_comments.first
       embedded_comment.body = 'newbody'
@@ -53,7 +53,7 @@ describe "Embedding" do
       expect(embedded_comments_reloaded.map(&:body)).to include('newbody')
     end
 
-    it "change attribute when saved through parent object" do
+    it 'change attribute when saved through parent object' do
       embedded_comments = EmbeddedComment.all
       embedded_comment = embedded_comments.first
       embedded_comment.body = 'newbody'
@@ -63,30 +63,30 @@ describe "Embedding" do
       expect(embedded_comments_reloaded.map(&:body)).to include('newbody')
     end
 
-    it "delete comment using integer" do
+    it 'delete comment using integer' do
     end
 
-    it "delete comment using integer string" do
+    it 'delete comment using integer string' do
     end
 
-    it "Count" do
+    it 'Count' do
       expect(EmbeddedComment.count).to eq 2
     end
   end
 
-  context "Creation of comment" do
+  context 'Creation of comment' do
     before do
       @post = Post.new
       @post.save
     end
 
-    it "not save when no parent is present" do
+    it 'not save when no parent is present' do
       comment = EmbeddedComment.new(body: 'no parent')
       expect(comment.save).to be false
       expect(comment.errors[:post]).to include('no_parent')
     end
 
-    it "save when initialized with parent actual name initialization" do
+    it 'save when initialized with parent actual name initialization' do
       comment = EmbeddedComment.new(body: 'no parent', post: @post)
       expect(comment.save).to be true
       expect(comment.post).to eq @post
@@ -96,7 +96,7 @@ describe "Embedding" do
       expect(reloaded_post.embedded_comments.include?(comment)).to be true
     end
 
-    it "save when initialized with parent object initialization" do
+    it 'save when initialized with parent object initialization' do
       comment = EmbeddedComment.new(body: 'no parent', parent_object: @post)
       expect(comment.save).to be true
       expect(comment.post).to eq @post
@@ -107,7 +107,7 @@ describe "Embedding" do
       expect(reloaded_post.embedded_comments.include?(comment)).to be true
     end
 
-    it "save when parent object is assigned later with relation name" do
+    it 'save when parent object is assigned later with relation name' do
       comment = EmbeddedComment.new(body: 'no parent')
       comment.post = @post
       expect(comment.save).to be true
@@ -118,7 +118,7 @@ describe "Embedding" do
       expect(reloaded_post.embedded_comments.include?(comment)).to be true
     end
 
-    it "save when parent object is assigned later with parent object assignment" do
+    it 'save when parent object is assigned later with parent object assignment' do
       comment = EmbeddedComment.new(body: 'no parent')
       comment.parent_object = @post
       expect(comment.save).to be true
@@ -130,7 +130,7 @@ describe "Embedding" do
     end
   end
 
-  context "belongs to strict_post" do
+  context 'belongs to strict_post' do
     before do
       @user = User.create(name: 'embedding user')
       @strict_post = StrictPost.create(user: @user)
@@ -142,14 +142,14 @@ describe "Embedding" do
       @post.save
     end
 
-    it "add embedded comments to strict_post" do
+    it 'add embedded comments to strict_post' do
       expect(@strict_post.save).to be true
       @post.embedded_comments.each { |ec| ec.strict_post = @strict_post; ec.save }
       strict_post_reloaded = StrictPost.find(@strict_post.id)
       expect(strict_post_reloaded.embedded_comments.size).to eq @post.embedded_comments.size
     end
 
-    it "have strict_post as association" do
+    it 'have strict_post as association' do
       expect(@strict_post.save).to be true
       @post.embedded_comments.each { |ec| ec.strict_post = @strict_post; ec.save }
       post_reloaded = Post.find(@post.id)
@@ -157,7 +157,7 @@ describe "Embedding" do
       expect(comment_reloaded.strict_post).to eq @strict_post
     end
 
-    it "have parent object when queried through relation" do
+    it 'have parent object when queried through relation' do
       expect(@strict_post.save).to be true
       @post.embedded_comments.each { |ec| ec.strict_post = @strict_post; ec.save }
       strict_post_reloaded = StrictPost.find(@strict_post.id)
@@ -165,7 +165,7 @@ describe "Embedding" do
       expect(strict_post_reloaded.embedded_comments.first.post).to eq @post
     end
 
-    it "have actual object same as in parent object" do
+    it 'have actual object same as in parent object' do
       expect(@strict_post.save).to be true
       @post.embedded_comments.each { |ec| ec.strict_post = @strict_post; ec.save }
       strict_post_reloaded = StrictPost.find(@strict_post.id)
--- spec/views_spec.rb
+++ spec/views_spec.rb
@@ -7,17 +7,17 @@ class CustomViewUser
   view :by_tags, type: SimplyCouch::Model::Views::ArrayPropertyViewSpec, key: :tags
 end
 
-describe "Custom couch views" do
-  context "with array views" do
-    it "finds objects with one match of the array" do
-      CustomViewUser.create(tags: ["agile", "cool", "extreme"])
-      CustomViewUser.create(tags: ["agile"])
-      expect(CustomViewUser.find_all_by_tags("agile").size).to eq 2
+describe 'Custom couch views' do
+  context 'with array views' do
+    it 'finds objects with one match of the array' do
+      CustomViewUser.create(tags: ['agile', 'cool', 'extreme'])
+      CustomViewUser.create(tags: ['agile'])
+      expect(CustomViewUser.find_all_by_tags('agile').size).to eq 2
     end
 
-    it "finds the object when the property is not an array" do
-      CustomViewUser.create(tags: "agile")
-      expect(CustomViewUser.find_all_by_tags("agile").size).to eq 1
+    it 'finds the object when the property is not an array' do
+      CustomViewUser.create(tags: 'agile')
+      expect(CustomViewUser.find_all_by_tags('agile').size).to eq 1
     end
   end
 end
